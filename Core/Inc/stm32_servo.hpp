ab3f3bdf6d781a596c6006b38f4d664fcfd67b41
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-12-17 05:26:51 +0900

    Add cpp and hpp files

---
+++ Core/Inc/stm32_servo.hpp
@@ -0,0 +1,15 @@
+/* 
+ * File:    stm32_servo.hpp
+ * version: 1.0.0
+ * Author:  Hajime
+ *
+ * Created on 2019/12/17
+ */
+
+class Stm32Servo {
+ private:
+  /* data */
+ public:
+  Stm32Servo(/* args */);
+  virtual ~Stm32Servo();
+};
---
+++ Core/Src/stm32_servo.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:    stm32_servo.cpp
+ * version: 1.0.0
+ * Author:  Hajime
+ *
+ * Created on 2019/12/17
+ */
+
+#include "stm32_servo.hpp"
+
+Stm32Servo::Stm32Servo(/* args */)
+{
+}
+
+Stm32Servo::~Stm32Servo()
+{
+}
