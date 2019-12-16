fd71ce7f76961d93bd3323a681f567b7a9690ab2
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-12-17 06:02:13 +0900

    Implement Stm32Servo class

--- Core/Inc/stm32_servo.hpp
+++ Core/Inc/stm32_servo.hpp
@@ -9,12 +9,29 @@
 #ifndef STM32_SERVO_HPP_
 #define STM32_SERVO_HPP_
 
+#include "main.h"
+
 class Stm32Servo {
+  struct Config {
+    uint32_t max_count;
+    double max_duty_rate;
+    double max_angle;
+    double min_duty_rate;
+    double min_angle;
+  };
+
  private:
-  /* data */
+  TIM_HandleTypeDef *htim_;
+  uint32_t channel_;
+  Config config_;
+  double angle_;
+
  public:
-  Stm32Servo(/* args */);
+  Stm32Servo(TIM_HandleTypeDef *, uint32_t, Config);
   virtual ~Stm32Servo();
+  double add_angle(double);
+  void set_angle(double);
+  double get_angle() const;
 };
 
 #endif // STM32_SERVO_HPP_
